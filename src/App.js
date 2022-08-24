8599a7e430d034aba1991ea947bd86bfe13fe72d
FarooqM1 <farooqdln97@gmail.com>
2022-08-24 14:35:52 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <header >
-        <BigScreen></BigScreen>
+        {/* <BigScreen></BigScreen> */}
+        <div> Test </div>
       </header>
     </div>
   );
