5fc19d41537be9e82a6b273ec64c1fc927000f38
FarooqM1 <farooqdln97@gmail.com>
2022-08-24 14:56:28 +0530

    removed responsiveness

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import EnhancedTable from './EnhancedTable';
 import { BigScreen } from './BigScreen';
 import { ContentDeliveryWithPagination } from './ContentDeliveryWithPagination';
 import { Assignment } from './Assignment';
+import { HeaderDiv } from './HeaderDiv';
+import StatsView from './StatsView';
 
 function App() {
   return (
     <div className="App">
       <header >
-        <BigScreen></BigScreen>
+        {/* <BigScreen></BigScreen> */}
+        <div> <StatsView></StatsView> </div> 
+            <div> <EnhancedTable />  </div>
       </header>
     </div>
   );
