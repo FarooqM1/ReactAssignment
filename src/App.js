5ae7a36e99d7bfc3892fdee96c2a161f1eef55f2
FarooqM1 <farooqdln97@gmail.com>
2022-08-24 14:43:24 +0530

    App deployed

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <header >
-        {/* <BigScreen></BigScreen> */}
-        <div> Test </div>
+        <BigScreen></BigScreen>
+        {/* <div> Test </div> */}
       </header>
     </div>
   );
